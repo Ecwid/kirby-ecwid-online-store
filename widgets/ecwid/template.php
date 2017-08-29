d973672f92696550b95984a6a91ac746132cc2ab
Vladimir Tarutko <marvin@ecwid.com>
2017-08-29 14:00:37 +0400

    Changed connect button appearance

--- widgets/ecwid/template.php
+++ widgets/ecwid/template.php
@@ -8,10 +8,11 @@
 <?php endif; ?>
 <div>
 	<?php if (Ecwid::get('storeID')): ?>
-    <a class="ecwid-connect ecwid-reconnect" href="javascript:void(0);">Disconnect and connect another store</a>    
+    <a class="ecwid-connect ecwid-reconnect ecwid-connect-link" href="javascript:void(0);">Disconnect and connect another store</a>    
     <?php else: ?>
     <a class="btn btn-rounded" href="https://my.ecwid.com/?partner=kirby#register">Create Free Store</a>  
-    <a class="btn btn-rounded ecwid-connect" href="javascript:void(0);">Connect Existing Ecwid Store</a>
+    <br/>
+    <a class="ecwid-connect ecwid-connect-link" href="javascript:void(0);">Connect Existing Ecwid Store</a>
     <?php endif; ?>
 </div>
     
