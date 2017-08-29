6af8d706c9f37ba0b7017f223f238ab1617cef63
Vladimir Tarutko <marvin@ecwid.com>
2017-08-29 14:51:45 +0400

    layout tweaks

--- widgets/ecwid/template.php
+++ widgets/ecwid/template.php
@@ -10,9 +10,9 @@
 	<?php if (Ecwid::get('storeID')): ?>
     <a class="ecwid-connect ecwid-reconnect ecwid-connect-link" href="javascript:void(0);">Disconnect and connect another store</a>    
     <?php else: ?>
-    <a class="btn btn-rounded" href="https://my.ecwid.com/?partner=kirby#register">Create Free Store</a>  
+    <a class="btn btn-rounded" target="_blank" href="https://my.ecwid.com/?partner=kirby#register">Create Free Store</a>  
     <br/>
-    <a class="ecwid-connect ecwid-connect-link" href="javascript:void(0);">Connect Existing Ecwid Store</a>
+    <a class="ecwid-connect ecwid-connect-existing ecwid-connect-link" href="javascript:void(0);">Connect Existing Ecwid Store</a>
     <?php endif; ?>
 </div>
     
